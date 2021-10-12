7a78bbfe826472519154dd823ab90794accc837c
Boris <47457848+boris-chb@users.noreply.github.com>
2021-10-12 15:55:52 +0100

    MaxChar solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -7,11 +7,21 @@
 
 function maxChar(str) {
   const charMap = {};
+  let max = 0;
+  let maxChar = "";
 
   for (let char of str) {
     !charMap[char] ? (charMap[char] = 1) : charMap[char]++;
   }
-  console.log(charMap);
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
 }
 
 console.log(maxChar("abcdseefadfsdfg"));
